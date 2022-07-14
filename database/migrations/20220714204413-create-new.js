ea62df2a9e2c2e9b9e8aa1cda92d44c77705a017
Roberto Berrocal <robertoberro20@gmail.com>
2022-07-14 15:53:36 -0500

    migrations and model new

---
+++ database/migrations/20220714204413-create-new.js
@@ -0,0 +1,43 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('News', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull:false
+      },
+      content: {
+        type: Sequelize.TEXT,
+        allowNull:false
+      },
+      image: {
+        type: Sequelize.STRING,
+        allowNull:false
+      },
+      //foreign key
+      categoryId: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      deletedAt: {
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('News');
+  }
+};
