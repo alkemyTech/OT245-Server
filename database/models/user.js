3c632aa8cce9987c0a19048336957f6cd837c24c
Gianni Maratta <marattagian15@gmail.com>
2022-07-14 17:53:43 -0300

    fixing user association in model

--- database/models/user.js
+++ database/models/user.js
@@ -11,7 +11,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
-      User.hasOne(models.Role, {
+      User.belongsTo(models.Role, {
         foreignKey: 'roleId'
       })
     }
