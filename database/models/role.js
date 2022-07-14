8a84be773e66e00942005c072e0dcdca5c8c16c2
Gianni Maratta <marattagian15@gmail.com>
2022-07-14 20:02:13 -0300

    association implemented in role model

--- database/models/role.js
+++ database/models/role.js
@@ -11,6 +11,9 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+      Role.hasMany(models.User, {
+        foreignKey: 'roleId'
+      })
     }
   };
   Role.init({
