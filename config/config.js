709bbab72873f01d0422e7edfcdd2c011042c26b
[PuenteMiguel] <[wilo1985@hotmail.com]>
2022-07-13 21:57:37 -0300

    database configuration

--- config/config.js
+++ config/config.js
@@ -1,21 +1,33 @@
-require('dotenv').config()
+require("dotenv").config();
 
 module.exports = {
   development: {
-    username: process.env.DB_USER,
+    username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
+    database: process.env.DB_DATABASE,
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
-    dialect: 'mysql',
+    dialect: process.env.DB_DIALECT,
+
+    seederStorage: "sequelize",
+    seederStorageTableName: "seeds",
+
+    migrationStorage: "sequelize",
+    migrationStorageTableName: "migrations",
   },
 
   production: {
-    username: process.env.DB_USER,
+    username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
+    database: process.env.DB_DATABASE,
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
-    dialect: 'mysql',
+    dialect: process.env.DB_DIALECT,
+
+    seederStorage: "sequelize",
+    seederStorageTableName: "seeds",
+
+    migrationStorage: "sequelize",
+    migrationStorageTableName: "migrations",
   },
-}
+};
