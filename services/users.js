e9500207fdf8cd93a4dd67eebbf30c44916aec1c
Gianni <68022732+marattagian@users.noreply.github.com>
2022-07-19 13:33:25 -0300

    [USERS] Endpoint users register OT245 - 24 (#14)
    * schema validator middleware created
    
    * user validation schema added
    
    * basic post handler for user controller
    
    * user registration service created
    
    * endpoint response, creating http error and calling service from controller
    
    * auth router created and added to the index router
    
    * Giving a roleId and fixing Model import
    
    * adding missing error message for a weak password
    
    * password encryptation with bcrypt implemented
    
    * fixing missed trailing commas
    
    * more specific schema name
    
    * reusability of finding users by email
    
    * fixing the users service naming
    
    * handling errors on validator middleware
    
    * naming fixed and email messages added
    
    * typo fix on schema import

---
+++ services/users.js
@@ -0,0 +1,27 @@
+const bcrypt = require('bcrypt')
+const { ErrorObject } = require('../helpers/error')
+const { User } = require('../database/models')
+
+exports.createUser = async (body) => {
+  try {
+    const existantUser = this.getUserByEmail(body.email)
+    if (existantUser) {
+      throw new ErrorObject('Email already in use', 404)
+    }
+    const hashedPassword = await bcrypt.hash(body.password, 10)
+    body.password = hashedPassword
+    body.roleId = 1
+    const newUser = await User.create(body)
+    if (!newUser) {
+      throw new ErrorObject('User registration failed', 404)
+    }
+    return newUser
+  } catch (error) {
+    throw new ErrorObject(error.message, error.statusCode || 500)
+  }
+}
+
+exports.getUserByEmail = async (email) => {
+  const user = await User.findOne({ where: { email } })
+  return user
+}
