7bbebfc46462c0641fbb9f89ef82634e17c61720
Gianni Maratta <marattagian15@gmail.com>
2022-07-18 21:06:54 -0300

    category model and migration

--- database/models/category.js
+++ database/models/category.js
@@ -1,35 +1,30 @@
-"use strict";
-
+'use strict';
+const {
+  Model
+} = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class Category extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
     static associate(models) {
+      // define association here
       Category.hasMany(models.New, {
-        foreignKey: "categoryId",
-      });
+        foreignKey: 'categoryId',
+      })
     }
-  }
-  Category.init(
-    {
-      name: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        validate: { len: [0, 50] },
-      },
-      description: {
-        type: DataTypes.STRING,
-        validate: { len: [0, 200] },
-      },
-      image: {
-        type: DataTypes.STRING,
-        validate: { len: [0, 300] },
-      },
-    },
-    {
-      sequelize,
-      modelName: "Category",
-      timestamps: true,
-      paranoid: true,
-    }
-  );
+  };
+  Category.init({
+    name: DataTypes.STRING,
+    description: DataTypes.STRING,
+    image: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'Category',
+    timestamps: true,
+    paranoid: true,
+  });
   return Category;
 };
