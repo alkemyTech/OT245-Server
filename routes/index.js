e4a801c2ddde251ce185fb6154fecf80a7064870
itsnicolearias <nicolmccann@gmail.com>
2022-07-19 10:37:48 -0300

    add index router for activities

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const { get } = require('../controllers/index')
+const activities = require('./activities')
 
 const router = express.Router()
 
 // example of a route with index controller get function
 router.get('/', get)
+router.use('/activities', activities)
 
 module.exports = router
