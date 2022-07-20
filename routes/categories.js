3a372f00ea904fa56732c3d0a590f8ec7bc68b6b
Gianni Maratta <marattagian15@gmail.com>
2022-07-20 14:53:55 -0300

    category delete route implemented

--- routes/categories.js
+++ routes/categories.js
@@ -1,10 +1,16 @@
 const express = require('express')
-const { get, getCategoryById, put } = require('../controllers/categories')
+const {
+  get,
+  getCategoryById,
+  put,
+  destroy,
+} = require('../controllers/categories')
 
 const router = express.Router()
 
 router.get('/', get)
 router.get('/:id', getCategoryById)
 router.put('/:id', put)
+router.delete('/:id', destroy)
 
 module.exports = router
