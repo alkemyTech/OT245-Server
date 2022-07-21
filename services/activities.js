9fc80540214b9b262c0aba422ad02675b1e49357
itsnicolearias <nicolmccann@gmail.com>
2022-07-20 22:15:18 -0300

    fix the response

--- services/activities.js
+++ services/activities.js
@@ -16,16 +16,16 @@ exports.updateActivity = async (req) => {
   try {
     const { id } = req.params
     const { name, content, image } = req.body
-    const activity = await Activity.findByPk(id)
-    if (!activity) {
-      throw new ErrorObject('Activity not found', 404)
-    }
-    const updatedActivity = await Activity.update({
+    await Activity.update({
       name,
       content,
       image,
     }, { where: { id } })
-    return updatedActivity
+    const activity = await Activity.findByPk(id)
+    if (!activity) {
+      throw new ErrorObject('Activity not found', 404)
+    }
+    return activity
   } catch (error) {
     throw new ErrorObject(error.message, error.statusCode || 500)
   }
