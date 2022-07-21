fdf6aa644300e4346f46dc2443125815654f82ea
Jorge Hinojosa <j.hinojosaacevedo@uandresbello.edu>
2022-07-21 19:08:25 -0400

    OT245-29-FIX

--- database/models/organization.js
+++ database/models/organization.js
@@ -11,7 +11,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
-      Category.hasMany(models.Slide, {
+      Organization.hasMany(models.Slide, {
         foreignKey: 'slideId',
       })
     }
