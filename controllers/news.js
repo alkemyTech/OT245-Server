67414d23720e67aa7725d1b336641dcfd0bfff4f
Gianni <68022732+marattagian@users.noreply.github.com>
2022-07-24 09:31:07 -0300

    [ENDPOINT] update news PUT /news/:id OT245 - 48 (#42)
    * update new and get new by id service
    
    * put handler for the news controller
    
    * update new route
    
    * error handling fixes
    
    * naming fix

--- controllers/news.js
+++ controllers/news.js
@@ -1,7 +1,7 @@
 const createHttpError = require('http-errors')
 const { endpointResponse } = require('../helpers/success')
 const { catchAsync } = require('../helpers/catchAsync')
-const { postNew } = require('../services/news')
+const { postNew, updateNew } = require('../services/news')
 
 module.exports = {
   post: catchAsync(async (req, res, next) => {
@@ -16,6 +16,23 @@ module.exports = {
       const httpError = createHttpError(
         error.statusCode,
         `[Error creating new] - [new - POST]: ${error.message}`,
+      )
+      next(httpError)
+    }
+  }),
+
+  put: catchAsync(async (req, res, next) => {
+    try {
+      const response = await updateNew(req)
+      endpointResponse({
+        res,
+        message: 'New updated successfully',
+        body: response,
+      })
+    } catch (error) {
+      const httpError = createHttpError(
+        error.statusCode,
+        `[Error updating new] - [news - PUT]: ${error.message}`,
       )
       next(httpError)
     }
--- routes/news.js
+++ routes/news.js
@@ -1,10 +1,11 @@
 const express = require('express')
-const { post } = require('../controllers/news')
+const { post, put } = require('../controllers/news')
 const { schemaValidator } = require('../middlewares/validator')
 const { news } = require('../schemas/news')
 
 const router = express.Router()
 
 router.post('/', schemaValidator(news), post)
+router.put('/:id', put)
 
 module.exports = router
