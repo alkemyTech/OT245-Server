0630aba3d59a99a38626206948b232318af3ba71
Mauri Di Pietro <dipietro.jm@gmail.com>
2022-07-25 11:19:37 -0300

    [MIDDLEWARE] verify user admin

--- middlewares/admin.js
+++ middlewares/admin.js
@@ -2,18 +2,21 @@ const createHttpError = require('http-errors')
 const { User } = require('../database/models')
 // const { Role } = require('../database/models')
 const { ErrorObject } = require('../helpers/error')
+const { decodeToken } = require('./jwt')
 
 exports.verifyAdmin = async (req, res, next) => {
   try {
-    const { email } = req.user
-    const user = await User.findOne({
+    const token = req.header('Authorization')
+    const user = await decodeToken(token)
+    console.log('user--->', user)
+    const isAdmin = await User.findOne({
       where: {
-        email,
+        email: user.email,
         roleId: 1,
       },
     })
 
-    if (user) {
+    if (isAdmin) {
       return next()
     }
     throw new ErrorObject('Unauthorized', 401)
