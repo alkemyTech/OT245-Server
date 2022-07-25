02edc51adb5e14c4a3ebc499d96a587be03ce6a1
Gianni <68022732+marattagian@users.noreply.github.com>
2022-07-25 10:44:46 -0300

    returning the user token in registration (#44)

--- services/users.js
+++ services/users.js
@@ -1,6 +1,7 @@
 const bcrypt = require('bcrypt')
 const { ErrorObject } = require('../helpers/error')
 const { User } = require('../database/models')
+const { generateToken } = require('../middlewares/jwt')
 
 exports.createUser = async (body) => {
   try {
@@ -15,7 +16,8 @@ exports.createUser = async (body) => {
     if (!newUser) {
       throw new ErrorObject('User registration failed', 404)
     }
-    return newUser
+    const token = await generateToken(newUser)
+    return token
   } catch (error) {
     throw new ErrorObject(error.message, error.statusCode || 500)
   }
