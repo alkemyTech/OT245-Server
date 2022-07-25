4bd4ba74c45c6f1965f5bced528cd9392db30d0d
Roberto Berrocal <88119333+R-Berrocal@users.noreply.github.com>
2022-07-25 13:56:40 -0500

    [user authentication] Welcome email sending - OT245 - 31 (#45)
    * template html added service postMail
    
    * email sending added to createUser service
    
    * fixed eslinter

--- services/sendgrid.js
+++ services/sendgrid.js
@@ -1,21 +1,21 @@
 const sgMail = require('@sendgrid/mail')
 const { ErrorObject } = require('../helpers/error')
+const { Organization } = require('../database/models')
+const { html } = require('../templates/welcomeEmail')
 require('dotenv').config()
 
 sgMail.setApiKey(process.env.SENDGRID_API_KEY)
 
 const postMail = async (email) => {
+  const organization = await Organization.findAll()
   const msg = {
     to: email,
     from: process.env.EMAILFROM,
-    subject: 'Sending with Twilio SendGrid is Fun',
-    text: 'and easy to do anywhere, even with Node.js',
-    html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+    subject: 'Bienvendio a alkemy',
+    html: html(organization[0]),
   }
   try {
     await sgMail.send(msg)
-
-    return msg
   } catch (error) {
     throw new ErrorObject(error.message, error.statusCode || 500)
   }
--- services/users.js
+++ services/users.js
@@ -2,6 +2,7 @@ const bcrypt = require('bcrypt')
 const { ErrorObject } = require('../helpers/error')
 const { User } = require('../database/models')
 const { generateToken } = require('../middlewares/jwt')
+const { postMail } = require('./sendgrid')
 
 exports.createUser = async (body) => {
   try {
@@ -16,6 +17,7 @@ exports.createUser = async (body) => {
     if (!newUser) {
       throw new ErrorObject('User registration failed', 404)
     }
+    await postMail(newUser.email)
     const token = await generateToken(newUser)
     return token
   } catch (error) {
