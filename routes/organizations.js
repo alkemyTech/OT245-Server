8d445714979805e643d3129e27ffdb3c48bc2da4
itsnicolearias <nicolmccann@gmail.com>
2022-07-25 19:15:58 -0300

    add verifyAdmin middleware

--- routes/organizations.js
+++ routes/organizations.js
@@ -1,11 +1,13 @@
 const { Router } = require('express')
 const { get, put } = require('../controllers/organizations')
+const { verifyAdmin } = require('../middlewares/admin')
+const { validateToken } = require('../middlewares/validateUser')
 const { schemaValidator } = require('../middlewares/validator')
 const { organization } = require('../schemas/organizations')
 
 const router = Router()
 
 router.get('/public', get)
-router.put('/public/:id', schemaValidator(organization), put)
+router.put('/public/:id', validateToken, verifyAdmin, schemaValidator(organization), put)
 
 module.exports = router
