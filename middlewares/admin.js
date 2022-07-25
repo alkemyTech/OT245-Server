7b41056eb8229ea49de6a2a5b7fa2fda7e2b643e
Mauri Di Pietro <dipietro.jm@gmail.com>
2022-07-25 13:41:49 -0300

    [MIDDLEWARE] verify user admin OT245-27

--- middlewares/admin.js
+++ middlewares/admin.js
@@ -1,6 +1,5 @@
 const createHttpError = require('http-errors')
 const { User } = require('../database/models')
-// const { Role } = require('../database/models')
 const { ErrorObject } = require('../helpers/error')
 const { decodeToken } = require('./jwt')
 
@@ -8,14 +7,12 @@ exports.verifyAdmin = async (req, res, next) => {
   try {
     const token = req.header('Authorization')
     const user = await decodeToken(token)
-    console.log('user--->', user)
     const isAdmin = await User.findOne({
       where: {
-        email: user.email,
+        email: user.user.email,
         roleId: 1,
       },
     })
-
     if (isAdmin) {
       return next()
     }
