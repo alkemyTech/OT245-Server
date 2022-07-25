962df7fa5e030d6d2c9f6952f0e38cdf2e801a31
Roberto Berrocal <88119333+R-Berrocal@users.noreply.github.com>
2022-07-25 17:28:51 -0500

    model and migration contact added (#50)

---
+++ database/migrations/20220725190821-create-contact.js
@@ -0,0 +1,36 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Contacts', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      phone: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      message: {
+        type: Sequelize.TEXT,
+        allowNull: false
+      },
+      deletedAt: {
+        allowNull: true,
+        type: Sequelize.DATE
+      },
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Contacts');
+  }
+};
