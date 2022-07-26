fd19c2ad204dcb2427cf301a3fd0fb0499dc1904
Gianni Maratta <marattagian15@gmail.com>
2022-07-26 10:12:07 -0300

    post contact service

---
+++ services/contacts.js
@@ -0,0 +1,27 @@
+const { ErrorObject } = require('../helpers/error')
+const { Contact } = require('../database/models')
+
+exports.postContact = async (body) => {
+  try {
+    const existentContact = await this.getContactByEmail(body.email)
+    if (existentContact) {
+      throw new ErrorObject('Email already registered', 404)
+    }
+    const contact = await Contact.create(body)
+    if (!contact) {
+      throw new ErrorObject('failed contact registration', 403)
+    }
+    return contact
+  } catch (error) {
+    throw new ErrorObject(error.message, error.statusCode || 500)
+  }
+}
+
+exports.getContactByEmail = async (email) => {
+  try {
+    const contact = await Contact.findOne({ where: { email } })
+    return contact
+  } catch (error) {
+    throw new ErrorObject(error.message, error.statusCode || 500)
+  }
+}
