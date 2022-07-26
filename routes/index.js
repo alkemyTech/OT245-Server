7f4530095a9167bcab52afae660464144623589c
Gianni Maratta <marattagian15@gmail.com>
2022-07-26 10:13:37 -0300

    post contacts route defined and added to index

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,7 @@ const categories = require('./categories')
 const organizations = require('./organizations')
 const users = require('./users')
 const news = require('./news')
+const contacts = require('./contacts')
 
 const router = express.Router()
 
@@ -17,5 +18,6 @@ router.use('/categories', categories)
 router.use('/organization', organizations)
 router.use('/users', users)
 router.use('/news', news)
+router.use('/contacts', contacts)
 
 module.exports = router
