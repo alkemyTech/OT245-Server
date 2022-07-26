ebc356693c4a6fa60c7aa4f7dbb02bfc7831ce6f
Gianni Maratta <marattagian15@gmail.com>
2022-07-26 20:23:31 -0300

    delete slide service

--- services/slides.js
+++ services/slides.js
@@ -11,3 +11,16 @@ exports.getSlides = async () => {
     throw new ErrorObject(error.message, error.statusCode || 500)
   }
 }
+
+exports.deleteSlide = async (id) => {
+  try {
+    const existentSlide = await Slide.findOne({ where: { id } })
+    if (!existentSlide || existentSlide.length === 0) {
+      throw new ErrorObject('Slide not found', 404)
+    }
+    const deletedSlide = await Slide.destroy({ where: { id } })
+    return deletedSlide
+  } catch (error) {
+    throw new ErrorObject(error.message, error.statusCode || 500)
+  }
+}
