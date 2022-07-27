450b837b086dce015548e46e50b979831fbe9854
Mauri Di Pietro <dipietro.jm@gmail.com>
2022-07-27 11:39:02 -0300

    [SLIDES] service update

--- services/slides.js
+++ services/slides.js
@@ -36,3 +36,22 @@ exports.deleteSlide = async (id) => {
     throw new ErrorObject(error.message, error.statusCode || 500)
   }
 }
+
+exports.updateSlide = async (req) => {
+  try {
+    const { id } = req.params
+    const { image, text, order } = req.body
+    await Slide.update({
+      image,
+      text,
+      order,
+    }, { where: { id } })
+    const slide = await Slide.findByPk(id)
+    if (!slide) {
+      throw new ErrorObject('Slide not found', 404)
+    }
+    return slide
+  } catch (error) {
+    throw new ErrorObject(error.message, error.statusCode || 500)
+  }
+}
