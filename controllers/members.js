be5def147736b57b308bfb7477c3498adfdb1a02
Roberto Berrocal <robertoberro20@gmail.com>
2022-07-28 15:58:22 -0500

    controller destroy added

--- controllers/members.js
+++ controllers/members.js
@@ -1,7 +1,7 @@
 const createHttpError = require('http-errors')
 const { endpointResponse } = require('../helpers/success')
 const { catchAsync } = require('../helpers/catchAsync')
-const { postMember } = require('../services/members')
+const { postMember, deleteMember } = require('../services/members')
 
 module.exports = {
   post: catchAsync(async (req, res, next) => {
@@ -16,6 +16,23 @@ module.exports = {
       const httpError = createHttpError(
         error.statusCode,
         `[Error creating member] - [members - POST]: ${error.message}`,
+      )
+      next(httpError)
+    }
+  }),
+  destroy: catchAsync(async (req, res, next) => {
+    try {
+      const { id } = req.params
+      const member = await deleteMember(id)
+      endpointResponse({
+        res,
+        message: 'User successfuly deleted',
+        body: member,
+      })
+    } catch (error) {
+      const httpError = createHttpError(
+        error.statusCode,
+        `[Error deleting member] - [member - DELETE]: ${error.message}`,
       )
       next(httpError)
     }
