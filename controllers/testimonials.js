9fbd855ce9c44f679c39cca0ed68fd6728bce008
Mauri Di Pietro <dipietro.jm@gmail.com>
2022-07-28 17:19:47 -0300

    [TESTIMONIALS] endpoint update OT245-70

--- controllers/testimonials.js
+++ controllers/testimonials.js
@@ -1,7 +1,7 @@
 const createHttpError = require('http-errors')
 const { endpointResponse } = require('../helpers/success')
 const { catchAsync } = require('../helpers/catchAsync')
-const { postTestimonial } = require('../services/testimonials')
+const { postTestimonial, updateTestimonial } = require('../services/testimonials')
 const { deleteTestimonial } = require('../services/testimonials')
 
 module.exports = {
@@ -22,33 +22,38 @@ module.exports = {
     }
   }),
 
-<<<<<<< HEAD
-  put: catchAsync(async (req, res, next) => {
-    try {
-      const response = await updateTestimonial(req)
-      endpointResponse({
-        res,
-        message: 'Testimonial updated successfully',
-=======
   destroy: catchAsync(async (req, res, next) => {
     try {
       const response = await deleteTestimonial(req.params.id)
       endpointResponse({
         res,
         message: 'Testimonial deleted successfully',
->>>>>>> 08f2c14f98744d35f76f5afd690f83d79d895e8a
         body: response,
       })
     } catch (error) {
       const httpError = createHttpError(
         error.statusCode,
-<<<<<<< HEAD
-        `[Error retrieving info] - [Testimonial - PUT]: ${error.message}`,
-=======
         `[Error deleting testimonial] - [testimonials - DELETE]: ${error.message}`,
->>>>>>> 08f2c14f98744d35f76f5afd690f83d79d895e8a
       )
       next(httpError)
     }
   }),
+
+  put: catchAsync(async (req, res, next) => {
+    try {
+      const response = await updateTestimonial(req)
+      endpointResponse({
+        res,
+        message: 'Testimonial updated successfully',
+        body: response,
+      })
+    } catch (error) {
+      const httpError = createHttpError(
+        error.statusCode,
+        `[Error retrieving info] - [Testimonial - PUT]: ${error.message}`,
+      )
+      next(httpError)
+    }
+  }),
+
 }
