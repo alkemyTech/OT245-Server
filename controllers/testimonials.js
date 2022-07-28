40fdcba0a764a42025e7303ead3943e965747a68
Nicole Arias <96268221+itsnicolearias@users.noreply.github.com>
2022-07-28 16:49:39 -0300

    [ENDPOINT] Delete testimonials OT245-71 (#67)
    * add backoffice route
    
    * add endpoint for delete testimonials
    
    * delete backoffice

--- controllers/testimonials.js
+++ controllers/testimonials.js
@@ -2,6 +2,7 @@ const createHttpError = require('http-errors')
 const { endpointResponse } = require('../helpers/success')
 const { catchAsync } = require('../helpers/catchAsync')
 const { postTestimonial } = require('../services/testimonials')
+const { deleteTestimonial } = require('../services/testimonials')
 
 module.exports = {
   post: catchAsync(async (req, res, next) => {
@@ -16,6 +17,23 @@ module.exports = {
       const httpError = createHttpError(
         error.statusCode,
         `[Error creating testimonial] - [testimonials - POST]: ${error.message}`,
+      )
+      next(httpError)
+    }
+  }),
+
+  destroy: catchAsync(async (req, res, next) => {
+    try {
+      const response = await deleteTestimonial(req.params.id)
+      endpointResponse({
+        res,
+        message: 'Testimonial deleted successfully',
+        body: response,
+      })
+    } catch (error) {
+      const httpError = createHttpError(
+        error.statusCode,
+        `[Error deleting testimonial] - [testimonials - DELETE]: ${error.message}`,
       )
       next(httpError)
     }
--- routes/testimonials.js
+++ routes/testimonials.js
@@ -1,12 +1,13 @@
-const express = require('express')
-const { schemaValidator } = require('../middlewares/validator')
-const { validateToken } = require('../middlewares/validateUser')
+const { Router } = require('express')
+const { destroy } = require('../controllers/testimonials')
 const { verifyAdmin } = require('../middlewares/admin')
+const { validateToken } = require('../middlewares/validateUser')
 const { postTestimonial } = require('../schemas/testimonials')
 const { post } = require('../controllers/testimonials')
+const { schemaValidator } = require('../middlewares/validator')
 
-const router = express.Router()
-
+const router = Router()
 router.post('/', [validateToken, verifyAdmin, schemaValidator(postTestimonial)], post)
+router.delete('/:id', [validateToken, verifyAdmin], destroy)
 
 module.exports = router
