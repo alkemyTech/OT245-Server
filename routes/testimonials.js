550587035404bcc1e73e11fea5671e0cd365e9b5
Mauri Di Pietro <74677356+MauriDiPietro@users.noreply.github.com>
2022-08-02 20:34:13 -0300

    [TESTIMONIALS] endpoint PUT - OT245-70 (#72)
    * [SLIDES] service update
    
    * [SLIDES] controller update
    
    * [SLIDES] controller update
    
    * [SLIDES] endpoint update OT245-60
    
    * [TESTIMONIALS] service update
    
    * [TESTIMONIALS] controller update
    
    * [TESTIMONIALS] endpoint put OT245-70
    
    * [TESTIMONIALS] endpoint update OT245-70

--- routes/testimonials.js
+++ routes/testimonials.js
@@ -3,11 +3,12 @@ const { destroy } = require('../controllers/testimonials')
 const { verifyAdmin } = require('../middlewares/admin')
 const { validateToken } = require('../middlewares/validateUser')
 const { postTestimonial } = require('../schemas/testimonials')
-const { post } = require('../controllers/testimonials')
+const { post, put } = require('../controllers/testimonials')
 const { schemaValidator } = require('../middlewares/validator')
 
 const router = Router()
 router.post('/', [validateToken, verifyAdmin, schemaValidator(postTestimonial)], post)
 router.delete('/:id', [validateToken, verifyAdmin], destroy)
+router.put('/:id', [validateToken, verifyAdmin], put)
 
 module.exports = router
