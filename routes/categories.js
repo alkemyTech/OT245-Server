7cba78f0985db217f19617ebd257e1398eb14758
Jorge Hinojosa <j.hinojosaacevedo@uandresbello.edu>
2022-08-03 12:00:46 -0400

    OT245-40 FIX - config env variables

--- routes/categories.js
+++ routes/categories.js
@@ -8,13 +8,14 @@ const {
   post,
   destroy,
 } = require('../controllers/categories')
+const { verifyAdmin } = require('../middlewares/admin')
 
 const router = express.Router()
 
 router.get('/', get)
 router.get('/:id', getCategoryById)
-router.post('/', schemaValidator(category), post)
-router.put('/:id', put)
-router.delete('/:id', destroy)
+router.post('/', verifyAdmin, schemaValidator(category), post)
+router.put('/:id', verifyAdmin, put)
+router.delete('/:id', verifyAdmin, destroy)
 
 module.exports = router
