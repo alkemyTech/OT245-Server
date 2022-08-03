b9dd6703bdfac9214d820c89ba14866bbabf5178
Mauri Di Pietro <dipietro.jm@gmail.com>
2022-08-03 09:38:02 -0300

    [COMMENT] create model and migration - OT245-79

---
+++ database/migrations/20220803105031-create-comment.js
@@ -0,0 +1,43 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Comments', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      body: {
+        type: Sequelize.STRING
+      },
+      userId: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'Users',
+          key: 'id',
+        },
+        allowNull: false,
+      },
+      // postId: {
+      //   type: Sequelize.INTEGER,
+      //   references: {
+      //     model: 'Posts',
+      //     key: 'id',
+      //   },
+      //   allowNull: false,
+      // },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Comments');
+  }
+};
