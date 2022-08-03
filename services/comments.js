cb1ae21eb996481cdb1dc288882876648eb4672d
Gianni Maratta <marattagian15@gmail.com>
2022-08-03 10:40:33 -0300

    list of comments service

---
+++ services/comments.js
@@ -0,0 +1,14 @@
+const { ErrorObject } = require('../helpers/error')
+const { Comment } = require('../database/models')
+
+exports.getComments = async () => {
+  try {
+    const comments = await Comment.findAll({
+      attributes: ['body'],
+      order: ['createdAt', 'DESC'],
+    })
+    return comments
+  } catch (error) {
+    throw new ErrorObject(error.message, error.statusCode || 500)
+  }
+}
