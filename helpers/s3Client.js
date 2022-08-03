c93678aab7f1686ad0cb2e1693267baf3f777432
Jorge Hinojosa <j.hinojosaacevedo@uandresbello.edu>
2022-08-03 17:32:03 -0400

    [Fix] s3Helper credentials

--- helpers/s3Client.js
+++ helpers/s3Client.js
@@ -4,6 +4,8 @@ const config = require('../config/config')
 // Create an Amazon S3 service client object.
 exports.s3Client = new S3Client({
   region: config.development.aws_region,
-  accessKeyId: config.development.aws_access_key_id,
-  secretAccessKey: config.development.aws_secret_access_key,
+  credentials: {
+    accessKeyId: config.development.aws_access_key_id,
+    secretAccessKey: config.development.aws_secret_access_key,
+  },
 })
