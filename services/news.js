42b4fe25dee979986ced3b15b9802443db2f5445
Gianni <68022732+marattagian@users.noreply.github.com>
2022-08-08 18:51:48 -0300

    [NEWS] Adding pagination OT245 - 86 (#93)
    * pagination service implemented and added to get news
    
    * get news controller
    
    * console log fixed
    
    * get news endpoint added to its router
    
    * linter fixes
    
    * more linter fixes
    
    * fix repited validateToken inport

--- services/news.js
+++ services/news.js
@@ -53,3 +53,55 @@ exports.deleteNew = async (id) => {
     throw new ErrorObject(error.message, error.statusCode || 500)
   }
 }
+
+exports.getNews = async (page) => {
+  try {
+    const limit = 10
+    const numberPage = page ? parseInt(page, 10) : 1
+    if (!Number.isInteger(numberPage) || numberPage <= 0) {
+      throw new ErrorObject('query param not valid', 500)
+    }
+    const offset = numberPage && numberPage >= 1 ? ((numberPage - 1) * limit) : 0
+    const currentPage = await New.findAll({ offset, limit })
+    const metadata = {}
+    const { previousPage, nextPage } = await this.pagination(limit, numberPage)
+    if (Object.keys(previousPage).length !== 0) {
+      metadata.previousPage = previousPage
+    }
+    if (Object.keys(nextPage).length !== 0) {
+      metadata.nextPage = nextPage
+    }
+    return { currentPage, metadata }
+  } catch (error) {
+    throw new ErrorObject(error.message, error.statusCode || 500)
+  }
+}
+
+exports.pagination = async (limit = 10, currentPage = 1) => {
+  try {
+    const previousPage = {}
+    const nextPage = {}
+    const newsAmount = await New.count()
+    const pagesAmount = Math.ceil(newsAmount / limit)
+    if (currentPage > pagesAmount) {
+      throw new ErrorObject('invalid page', 500)
+    }
+    if (currentPage > 1) {
+      previousPage.url = `/news?page=${currentPage - 1}`
+      previousPage.content = await New.findAll({
+        offset: ((currentPage - 2) * limit),
+        limit,
+      })
+    }
+    if (currentPage < pagesAmount) {
+      nextPage.url = `/news?page=${currentPage + 1}`
+      nextPage.content = await New.findAll({
+        offset: (currentPage * limit),
+        limit,
+      })
+    }
+    return { previousPage, nextPage }
+  } catch (error) {
+    throw new ErrorObject(error.message, error.statusCode || 500)
+  }
+}
