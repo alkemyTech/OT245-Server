af4441e91c4901db0ca86870c4cc043a45cf871e
Jorge Hinojosa Acevedo <38705539+HinojosaAcevedoJ@users.noreply.github.com>
2022-08-16 22:38:30 -0400

    [News] Documentation (#111)
    * [News] Documentation
    
    * [News] Documentation

--- controllers/news.js
+++ controllers/news.js
@@ -10,6 +10,278 @@ const {
   getCommentsByNewId,
 } = require('../services/news')
 
+/**
+ * @swagger
+ * components:
+ *  schemas:
+ *    New:
+ *      type: object
+ *      properties:
+ *        id:
+ *          type: integer
+ *        name:
+ *          type: string
+ *        image:
+ *          type: string
+ *        type:
+ *          type: string
+ *        createdAt:
+ *          type: integer
+ *          format: date
+ *        updatedAt:
+ *          type: integer
+ *          format: date
+ *        deletedAt:
+ *          type: integer
+ *          format: date
+ *      example:
+ *        name: new 1
+ *        image: https://image.com/new1.jpg
+ *        type: news
+ *        createdAt: 2022-08-16T08:30:55.77
+ *        updatedAt: 2022-08-16T08:30:55.77
+ *        deletedAt: null
+ */
+
+/**
+ * @swagger
+ * /news:
+ *  post:
+ *    tags: [New]
+ *    summary: Create news
+ *    requestBody:
+ *      required: true
+ *      content:
+ *        application/json:
+ *          schema:
+ *            type: object
+ *            $ref: '#/components/schemas/New'
+ *    responses:
+ *      200:
+ *        description: New created successfuly
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ *              properties:
+ *                status:
+ *                  type: boolean
+ *                code:
+ *                  type: integer
+ *                message:
+ *                  type: string
+ *                body:
+ *                  type: object
+ *                  properties:
+ *                    id:
+ *                      type: integer
+ *                    name:
+ *                      type: string
+ *                    image:
+ *                      type: string
+ *                    type:
+ *                      type: string
+ *                    createdAt:
+ *                      type: string
+ *                      format: date-time
+ *                    updatedAt:
+ *                      type: string
+ *                      format: date-time
+ *                    deletedAt:
+ *                      type: string
+ *                      format: date-time
+ *              example:
+ *                status: true
+ *                code: 200
+ *                message: New created successfuly
+ *                body:
+ *                  id: 1
+ *                  name: New 1
+ *                  image: https://image.com/new1.jpg
+ *                  type: news
+ *                  createdAt: 2022-08-16T08:30:55.77
+ *                  updatedAt: 2022-08-16T08:30:55.77
+ *      401:
+ *        description: Unauthorized user
+ *      403:
+ *        description: Token is required
+ *      500:
+ *        description: Internal server error
+ */
+
+/**
+ * @swagger
+ * /news/{id}:
+ *  put:
+ *    tags: [New]
+ *    summary: Update newss
+ *    parameters:
+ *    - in: path
+ *      name: id
+ *    schema:
+ *      type: integer
+ *      required: true
+ *      description: ID numeric of New to update.
+ *    requestBody:
+ *      required: true
+ *      content:
+ *        application/json:
+ *          schema:
+ *            type: object
+ *            $ref: '#/components/schemas/New'
+ *    responses:
+ *      200:
+ *        description: New updated successfuly
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ *              properties:
+ *                status:
+ *                  type: boolean
+ *                code:
+ *                  type: integer
+ *                message:
+ *                  type: string
+ *                body:
+ *                  type: object
+ *                  properties:
+ *                    id:
+ *                      type: integer
+ *                    name:
+ *                      type: string
+ *                    image:
+ *                      type: string
+ *                    type:
+ *                      type: string
+ *                    createdAt:
+ *                      type: string
+ *                      format: date-time
+ *                    updatedAt:
+ *                      type: string
+ *                      format: date-time
+ *                    deletedAt:
+ *                      type: string
+ *                      format: date-time
+ *              example:
+ *                status: true
+ *                code: 200
+ *                message: New updated successfuly
+ *                body:
+ *                  id: 1
+ *                  name: New 1 updated
+ *                  image: https://image.com/new1.jpg
+ *                  type: news
+ *                  createdAt: 2022-08-16T08:30:55.77
+ *                  updatedAt: 2022-08-16T08:50:00.71
+ *      401:
+ *        description: Unauthorized user
+ *      403:
+ *        description: Token is required
+ *      404:
+ *        description: New not found
+ *      500:
+ *        description: Internal server error
+ */
+
+/**
+ * @swagger
+ * /news/{id}:
+ *  delete:
+ *    tags: [New]
+ *    summary: Delete new
+ *    parameters:
+ *    - in: path
+ *      name: id
+ *    schema:
+ *      type: integer
+ *      required: true
+ *      description: ID numeric of New to delete.
+ *    responses:
+ *      200:
+ *        description: New successfuly deleted
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ *              properties:
+ *                status:
+ *                  type: boolean
+ *                code:
+ *                  type: integer
+ *                message:
+ *                  type: string
+ *                body:
+ *                  type: object
+ *                  properties:
+ *                    id:
+ *                      type: integer
+ *                    name:
+ *                      type: string
+ *                    image:
+ *                      type: string
+ *                    type:
+ *                      type: string
+ *                    createdAt:
+ *                      type: string
+ *                      format: date-time
+ *                    updatedAt:
+ *                      type: string
+ *                      format: date-time
+ *                    deletedAt:
+ *                      type: string
+ *                      format: date-time
+ *              example:
+ *                status: true
+ *                code: 200
+ *                message: New updated successfuly
+ *                body:
+ *                  id: 1
+ *                  name: New 1 updated
+ *                  image: https://image.com/new1.jpg
+ *                  type: news
+ *                  createdAt: 2022-08-16T08:30:55.77
+ *                  updatedAt: 2022-08-16T08:30:55.77
+ *                  deletedAt: 2022-08-16T09:30:55.77
+ *      401:
+ *        description: Unauthorized user
+ *      403:
+ *        description: Token is required
+ *      404:
+ *        description: New not found
+ *      500:
+ *        description: Internal server error
+ */
+
+/**
+ * @swagger
+ * /news:
+ *  get:
+ *    tags: [New]
+ *    summary: List of news.
+ *    responses:
+ *      200:
+ *        description: List of news in database.
+ *        content:
+ *          application/json:
+ *            schema:
+ *            type: array
+ *            items:
+ *              properties:
+ *                id:
+ *                  type: integer
+ *                  example: 1
+ *                name:
+ *                  type: string
+ *                  example: New 1
+ *                image:
+ *                  type: string
+ *                  example: https://image.com/new1.jpg
+ *                type:
+ *                  type: string
+ *                  example: news
+ */
+
 module.exports = {
   post: catchAsync(async (req, res, next) => {
     try {
